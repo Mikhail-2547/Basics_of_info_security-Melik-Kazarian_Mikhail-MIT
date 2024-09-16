1600c90f71a42b53bfaa9f8953f8dc868c01520f
Mikhail-2547 <artiprice@gmail.com>
2024-09-16 16:25:27 +0300

    Laba_2

--- Projects/Lab_2/Ex_1/Program.cs
+++ Projects/Lab_2/Ex_1/Program.cs
@@ -1,1 +1,13 @@
 ﻿using System;
+
+const string file_name = "decFileName.txt";
+const string enc_file_name_0 = "encfile3.dat";
+const string enc_file_name = "encfile5.dat";
+
+
+string password = "password";
+
+
+
+Console.WriteLine(Ex_1.Encrypting.file_writing_str(enc_file_name_0, Ex_1.Encrypting.file_reading_decrypted(enc_file_name)));
+Console.WriteLine();
