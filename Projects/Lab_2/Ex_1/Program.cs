5ff7df8935ffe1884b8241438e79a09a8a24e58a
Mikhail-2547 <pechenkaslivky@gmail.com>
2024-09-23 07:27:03 +0300

    Lab_3

--- Projects/Lab_2/Ex_1/Program.cs
+++ Projects/Lab_2/Ex_1/Program.cs
@@ -1,12 +1,22 @@
 ﻿using Ex_1;
 using System;
 using System.Xml.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 const string file_name = "encfile5.dat";
+const string key = "I1g+4";
 const string known_word = "Mit21";
 
 
-Console.WriteLine("Decrypted: " + Ex_1.Encrypting.decryption(Encrypting.file_reading(file_name), known_word));
+/*Console.WriteLine("Decrypted: " + Ex_1.Encrypting.decryption(Encrypting.file_reading(file_name), known_word));
+*/
+
+/*Ex_1.Encrypting.string_to_five(Ex_1.Encrypting.file_reading(file_name), known_word);
+*/
+
+Console.WriteLine(Ex_1.Encrypting.file_reading_decrypted(Ex_1.Encrypting.encrypt_procedures(Ex_1.Encrypting.file_reading(file_name), Ex_1.Encrypting.string_to_bytes(key))));
+
+
 
 
 /*string message = "145965561", key = "2asdaasda";
@@ -24,3 +34,7 @@ Console.WriteLine("Message: "+ message);
 
 Console.WriteLine("Decrypted: "+ Ex_1.Encrypting.file_reading_decrypted(decdata));
 Console.WriteLine("Encrypted: "+ Ex_1.Encrypting.file_reading_decrypted(encdata));*/
+/*g + 4I1
+    g+4I1
+g + 4I1x
+*/
